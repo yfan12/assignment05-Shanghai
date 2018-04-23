8857fdc2673a2dd012e7c0298008a22f4d53f67d
Zhenghao Yang <zyang27@u.rochester.edu>
2018-04-22 21:19:05 -0400

    got it

---
+++ assignment05-shanghai/home.php
@@ -0,0 +1,35 @@
+<?php include('inc/html-top.inc');?>
+
+<body class = "z-pattern">
+	<header class="persistent">
+		<div class="container">
+			<div class="primary">
+				<a href="home.php"><img src="images/icon.png" alt="Coffee"></a>
+			</div>
+			<div class="strong">
+				<h3><a href="https://en.wikipedia.org/wiki/Sushi" target="_blank"><em>Learn more about Sushi on WikiPedia!</em></a></h3>
+			</div>
+		</div>
+	</header>
+
+	<main>
+		<div class="title">
+			<h1><strong>Sushi</strong></h1>
+			<div class="subtitle"><strong>All for art</strong></div>
+		</div>
+
+	</main>
+
+	<footer class="persistent">
+		<div class="container">
+			<div class="weak">
+				<h3><em>Presentation of cuisine is<br>
+				an aesthetic quality</em></h3>
+			</div>
+			<div class="terminal">
+				<h3><a href="coffee.php"><em>Go to Sushipedia...</em></a></h3>
+			</div>
+		</div>
+	</footer>
+</body>
+</html>
